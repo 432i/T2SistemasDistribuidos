87656155006ca7b1ef33fa963bff4c7b86b0df13
ethielc <ethielcarmona@gmail.com>
2020-11-23 14:59:44 -0300

    Prueba 1, conexion DN con Cli

--- dataNode/dataNode1.go
+++ dataNode/dataNode1.go
@@ -9,9 +9,24 @@ import(
         "time"
         "golang.org/x/net/context"
         "google.golang.org/grpc"
-        "github.com/432i/T1SisDistribuidos/logistica/chat"
+		"github.com/432i/T2SistemasDistribuidos/dependencias/chatclidn"
+		pb "connclidn"
 )
 
-func main(){
+func serverDN1() {
+	//--------------------------------------------------------------> Server1
+	fmt.Print("Creando conexion...")
+	lis, err := net.Listen("tcp", ":50001")
+	if err != nil {
+		log.Fatalf("failed to listen2: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterChatCliDnServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve s2: %v", err)
+	}
+}
 
+func main(){
+	go serverDN1()
 }
