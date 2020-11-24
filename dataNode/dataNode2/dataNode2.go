3b2811934d88e324897768af97ef7c9eaac5b33e
ethielc <ethielcarmona@gmail.com>
2020-11-24 16:27:26 -0300

    Datanodes

---
+++ dataNode/dataNode2/dataNode2.go
@@ -0,0 +1,58 @@
+package main
+import(
+    	//"os"
+        //"strings"
+        //"io"
+        //"encoding/csv"
+        "log"
+        "fmt"
+		//"time"
+		"net"
+        "golang.org/x/net/context"
+        "google.golang.org/grpc"
+		pb "github.com/432i/T2SistemasDistribuidos/dependencias/serverclidn"
+)
+
+type Server struct {
+	pb.UnimplementedChatCliDnServer
+}
+
+func (s *Server) ChunkaDN(ctx context.Context, chunkcito *pb.Chunk) (*pb.Message, error) {
+	
+	msj := pb.Message{
+		Body: "ok",
+	}
+	return &msj, nil
+}
+
+func serverDN1() {
+	//-----------------------------------------------------------------> Server1
+	lis, err := net.Listen("tcp", ":50001")
+	if err != nil {
+		log.Fatalf("failed to listen2: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterChatCliDnServer(s, &Server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve s2: %v", err)
+	}
+}
+
+func main(){
+	var respuesta string
+	go serverDN1()
+	fmt.Print("El servidor de Data Node 1 esta activo\n")
+	fmt.Println("Ingrese 432 y presione Enter para salir del programa")
+	for{
+		_, err := fmt.Scanln(&respuesta)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		if strings.Compare("432", respuesta) == 0{
+			fmt.Println("Saliendo del programa. . . ")
+			break
+		}
+		fmt.Println("que")
+	}
+}
