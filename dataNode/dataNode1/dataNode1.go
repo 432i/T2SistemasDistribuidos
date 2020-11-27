0dc0692b21f729f80772018ee7f58ec77b26b883
ethielc <ethielcarmona@gmail.com>
2020-11-27 14:19:50 -0300

    Logica DN terminada (? falta ver el algoritmo de gestión para distribuido y centralizado

--- dataNode/dataNode1/dataNode1.go
+++ dataNode/dataNode1/dataNode1.go
@@ -16,97 +16,141 @@ import(
 type Server struct {
 	pb.UnimplementedChatCliDnServer
 }
-/*
 
-type ChunkInterno struct {
-	NombreLibro string
-	TotalPartes string
-	Parte string
-	Datos bytes;
-}
+var cola_chunks_de_cliente []pb.Chunk
 
-var cola_chunks_de_cliente []ChunkInterno
-var ultima_parte int
-
-func almacenarChunk(chunkcito *pb.Chunk) {
-	// write to disk
-	fileName := chunkcito.nombreLibro + "_" + chunkcito.Parte
-
+func almacenarChunk(chunkcito pb.Chunk) {
+	fileName := chunkcito.nombreLibro + "#" + chunkcito.Parte
 	_, err := os.Create(fileName)
 	if err != nil {
 			fmt.Println("Error al crear el archivo del chunk")
 			os.Exit(1)
 	}
-
-	// write/save buffer to disk
 	ioutil.WriteFile(fileName, chunkcito.Datos, os.ModeAppend)
 }
 
-func (s *Server) ChunkaDN(ctx context.Context, chunkcito *pb.Chunk) (*pb.Message, error) {
-	trozo := ChunkInterno {
-		NombreLibro: chunkcito.GetNombreLibro,
-		TotalPartes: chunkcito.GetTotalPartes,
-		Parte: chunkcito.GetParte,
-		Datos: chunkcito.GetDatos,
+func propuestaEntreTres(c2 pb.NewChatCliDnClient, c3 pb.NewChatCliDnClient) {
+	msg2 := pb.Message {
+		body: "m",
 	}
-	cola_chunks_de_cliente = append(cola_chunks_de_cliente, trozo)
-	
-	if chunkcito.Parte == chunkcito.TotalPartes {
-		ultima_parte = 1
-	} else {
-		ultima_parte = 0
+	msg3 := pb.Message {
+		body: "m",
+	}
+	i, _ := strconv.Atoi(cola_chunks_de_cliente[0].parte)
+	chunkcito := pb.Chunk {
+		nombreLibro: cola_chunks_de_cliente[0].nombreLibro,
+		totalPartes: cola_chunks_de_cliente[0].totalPartes,
+		parte: cola_chunks_de_cliente[0].parte,
+		datos: cola_chunks_de_cliente[0].datos,
+		algoritmo: cola_chunks_de_cliente[0].algoritmo,
 	}
 
-	msj := pb.Message{
-		Body: "Chunk recibido:\n    {nombreLibro: " + trozo.nombreLibro + ",\n    totalPartes: " + trozo.totalPartes + ",\n    parte: " + trozo.parte + ",\n    datos: " + trozo.datos + "}",
+	if cola_chunks_de_cliente[0].parte == "1" {
+		almacenarChunk(chunkcito)
 	}
-	return &msj, nil
-}*/
-func (s *Server) ChunkaDN(stream pb.Chunk) error {
-
-	for {
-	  chunk, err := stream.Recv()
-	  if err == io.EOF {
-		return stream.SendAndClose(&pb.Message{
-		  body: "ok",
-		})
-	  }
-	  if err != nil {
-		return err
-	  }
+	if cola_chunks_de_cliente[0].parte == "2" {
+		msg2, _ = c2.ChunkEntreDN(context.Background(), &chunkcito)
+		fmt.Println(msg2.body)
+	}
+	if cola_chunks_de_cliente[0].parte == "3" {
+		msg3, _ = c3.ChunkEntreDN(context.Background(), &chunkcito)
+		fmt.Println(msg3.body)
+	}
+	if i > 3 {
+		j := rand.Intn(3)
+		if j == 0 {
+			almacenarChunk(chunkcito)
+		} else if j == 1 {
+			msg2, _ = c2.ChunkEntreDN(context.Background(), chunkcito)
+			fmt.Println(msg2.body)
+		} else {
+			msg3, _ = c3.ChunkEntreDN(context.Background(), chunkcito)
+			fmt.Println(msg3.body)
+		}
 	}
 }
 
-func propuesta() {
-	var proposal;
-	if chunkcito.totalPartes == 3 {
-		proposal = 0
-	} else if chunkcito.totalPartes < 3 {
-		proposal = 1
-	} else {
-		proposal = 2
+func propuestaEntreDos(c pb.NewChatCliDnClient) {
+	msg := pb.Message {
+		body: "m",
 	}
-	return proposal
+	i, _ := strconv.Atoi(cola_chunks_de_cliente[0].parte)
+	chunkcito := pb.Chunk {
+		nombreLibro: cola_chunks_de_cliente[0].nombreLibro,
+		totalPartes: cola_chunks_de_cliente[0].totalPartes,
+		parte: cola_chunks_de_cliente[0].parte,
+		datos: cola_chunks_de_cliente[0].datos,
+		algoritmo: cola_chunks_de_cliente[0].algoritmo,
+	}
+
+	if cola_chunks_de_cliente[0].parte == "1" {
+		almacenarChunk(chunkcito)
+	}
+	if cola_chunks_de_cliente[0].parte == "2" {
+		msg, _ = c.ChunkEntreDN(context.Background(), &chunkcito)
+		fmt.Println(msg.body)
+	}
+	if i > 2 {
+		j := rand.Intn(2)
+		if j == 0 {
+			almacenarChunk(chunkcito)
+		} else {
+			msg, _ = c.ChunkEntreDN(context.Background(), chunkcito)
+			fmt.Println(msg.body)
+		}
+	}
 }
 
-func generarPropuesta(prop int, direccion string) {
-	var connDN2 *grpc.ClientConn
-	connDN2, err := grpc.Dial(direccion, grpc.WithInsecure())
-	if err != nil {
-			return "no"
+func generarPropuesta(cantPartes string) {
+	var se_pudo2, se_pudo3 bool
+	se_pudo2 = true
+	se_pudo3 = true
+	partes, _:= strconv.Atoi(cantPartes)
+	var connDN2, connDN3 *grpc.ClientConn
+	i := 0
+	for i < partes {
+		connDN2, err2 := grpc.Dial(direccion, grpc.WithInsecure())
+		if err2 != nil {
+			se_pudo2 = false
+		}
+		defer connDN2.Close()
+		c2 := pb.NewChatCliDnClient(connDN2)
+		fmt.Println("Conexion realizada correctamente con el Data Node de IP 10.6.40.150")
+
+		connDN3, err3 := grpc.Dial(direccion, grpc.WithInsecure())
+		if err3 != nil {
+			se_pudo3 = false
+		}
+		defer connDN3.Close()
+		c3 := pb.NewChatCliDnClient(connDN2)
+		fmt.Println("Conexion realizada correctamente con el Data Node de IP 10.6.40.151")
+		
+		if (se_pudo2 == true && se_pudo3 == true) {
+			propuestaEntreTres(c2, c3, prop)
+		} else if (se_pudo2 == true && se_pudo3 == false) {
+			propuestaEntreDos(c2, prop)
+		} else if (se_pudo2 == false && se_pudo3 == true) {
+			propuestaEntreDos(c3, prop)
+		} else {
+			almacenarChunk(cola_chunks_de_cliente[0])
+		}
+
+		if len(cola_chunks_de_cliente) == 1 {
+			cola_chunks_de_cliente = make([]pb.Chunk, 0)
+		} else {
+			cola_chunks_de_cliente = cola_chunks_de_cliente[1:]
+		}
+		se_pudo2 = true
+	se_pudo3 = true
+		i++
 	}
-	defer conn.Close()
-	c := pb.NewChatCliDnClient(connDN2)
-	fmt.Println("Conexion realizada correctamente con el Data Node de IP "+ip+"\n")
-	return c
 }
 
-func chunksRecibidos() {
+func escucharListaChunks() {
 	var prop int
 	for { 
-		if ultima_parte == 1 {
-			prop = propuesta()
-			generarPropuesta(prop, "10.6.40.150:50001")
+		if len(cola_chunks_de_cliente) != 0 {
+			generarPropuesta(cola_chunks_de_cliente[0].totalPartes)
 		}
 	}
 }
@@ -126,11 +170,36 @@ func serverDN1() { //Comunicacion con cliente
 	}
 }
 
+func (s *Server) ChunkaDN(stream pb.Chunk) error {
+	for {
+		chunk, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Message {
+			body: "Stream recibido",
+			})
+		}
+		if err != nil {
+			return err
+		}
+		cola_chunks_de_cliente = append(cola_chunks_de_cliente, chunk)
+	}
+}
+
+func (s *Server) ChunkEntreDN(ctx context.Context, chunkcito *pb.Chunk) (*pb.Message, error) {
+	almacenarChunk(chunkcito)
+	fnt.Println("Se ha almacenado el chunk:\n    {nombreLibro: " + chunkcito.nombreLibro + ",\n    totalPartes: " + chunkcito.totalPartes + ",\n    parte: " + chunkcito.parte + ",\n    datos: " + chunkcito.datos + "}",
+
+	msj := pb.Message{
+		Body: "Chunk recibido y almacenado:\n    {nombreLibro: " + chunkcito.nombreLibro + ",\n    totalPartes: " + chunkcito.totalPartes + ",\n    parte: " + chunkcito.parte + ",\n    datos: " + chunkcito.datos + "}",
+	}
+	return &msj, nil
+}
+
 func main(){
 	fmt.Println("El servidor de Data Node 1")
 	var respuesta string
 	go serverDN1()
-	go chunksRecibidos()
+	go escucharListaChunks()
 	fmt.Println("Ingrese 432 y presione Enter para salir del programa")
 	for{
 		_, err := fmt.Scanln(&respuesta)
